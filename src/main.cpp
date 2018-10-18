9b08ebbc7647441e4053e154072aa185b787f69a
Josef Adamcik <josef.adamcik@gmail.com>
2018-10-18 09:29:21 +0200

    We can now program and dump the memory

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,6 @@ static const uint8_t bufferSize = 16;
 static const unsigned int maxAddress = 0x7FFF;
 byte buffer[bufferSize];
 
-// #define SHIFT_DATA 2
-// #define SHIFT_CLK 3
-// #define SHIFT_LATCH 4
-// #define EEPROM_D0 5
-// #define EEPROM_D7 12
-// #define WRITE_EN 13
-
 /*
  * Output the address bits and outputEnable signal using shift registers.
  */
@@ -30,14 +23,11 @@ void setAddress(unsigned int address, bool outputEnable) {
 }
 
 
-
-
 void initIoPins(uint8_t mode) {
   for (int pin = eepromIo0; pin <= eepromIo7; pin += 1) {
     pinMode(pin, mode);
   }
 }
-
 
 void initIoInput() {
   initIoPins(INPUT);
@@ -65,9 +55,8 @@ byte readEEPROM(unsigned int address) {
  * Write a byte to the EEPROM at the specified address.
  */
 void writeEEPROM(unsigned int address, byte data) {
-  setAddress(address, /*outputEnable*/ false);
   initIoOutput();
-
+  setAddress(address, /*outputEnable*/ false);
   for (int pin = eepromIo0; pin <= eepromIo7; pin += 1) {
     digitalWrite(pin, data & 1);
     data = data >> 1;
@@ -83,14 +72,14 @@ void writeEEPROM(unsigned int address, byte data) {
  * Read the contents of the EEPROM and print them to the serial monitor.
  */
 void dumpContents() {
-  for (unsigned int base = 0; base <= maxAddress / 16; base += 16) {
+  for (unsigned int base = 0; base <= maxAddress; base += 16) {
     byte data[16];
     for (unsigned int offset = 0; offset <= 15; offset += 1) {
       data[offset] = readEEPROM(base + offset);
     }
 
     char buf[80];
-    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+    sprintf(buf, "%04x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
             base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
             data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
 
@@ -123,17 +112,7 @@ void printHelp() {
   Serial.println("======");
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(shiftDataPin, OUTPUT);
-  pinMode(shiftClkPin, OUTPUT);
-  pinMode(shiftLatchPin, OUTPUT);
-  digitalWrite(eepromWriteEn, HIGH);
-  pinMode(eepromWriteEn, OUTPUT);
-  
-  Serial.begin(57600);
-  printHelp();
-}
+
 
 int hexToNum(byte hex) {
   if (hex >= 'a' && hex <= 'f') {
@@ -165,11 +144,7 @@ void writeFromSerial() {
   Serial.print("w");
   unsigned int address = readAddressFromSerial();
   //read adress
-  Serial.println();
-  Serial.print(maxAddress, HEX); Serial.println();
-  Serial.print(address, HEX); Serial.println();
-  Serial.println(address > maxAddress - 16);
-  if (address > maxAddress - 16) {
+  if (address > maxAddress - 15) {
     Serial.println("=E:Address out of range");
     return;
   }
@@ -205,6 +180,18 @@ void writeFromSerial() {
   }
   Serial.println();
   Serial.println("=DONE");
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(shiftDataPin, OUTPUT);
+  pinMode(shiftClkPin, OUTPUT);
+  pinMode(shiftLatchPin, OUTPUT);
+  digitalWrite(eepromWriteEn, HIGH);
+  pinMode(eepromWriteEn, OUTPUT);
+  
+  Serial.begin(57600);
+  printHelp();
 }
 
 void loop() {
